67b470e2b4cb6b7b952d8e1a74e68a6de13dff12
lim ho <dlagh123@gmail.com>
2024-01-15 13:01:19 +0900

    리액트쿼리 셋팅

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -4,19 +4,47 @@ import Tab from "./_component/Tab";
 import TabProvider from "./_component/TabProvider";
 import Post from "../_component/Post";
 import PostForm from "./_component/PostForm";
-export default function Home() {
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+
+async function getPostRecommend() {
+  const res = await fetch("http://localhost:9090/api/postRecommends", {
+    next: {
+      tags: ["posts", "recommends"],
+    },
+    cache: "no-store",
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+  return res.json();
+}
+
+export default async function Home() {
+  const queryClient = new QueryClient();
+  await queryClient.prefetchQuery({
+    queryKey: ["posts", "recomends"],
+    queryFn: getPostRecommend,
+  });
+  const dehydratedState = dehydrate(queryClient);
+
   return (
     <main className={style.main}>
-      <TabProvider>
-        <Tab />
-        <PostForm />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </TabProvider>
+      <HydrationBoundary state={dehydratedState}>
+        <TabProvider>
+          <Tab />
+          <PostForm />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+          <Post />
+        </TabProvider>
+      </HydrationBoundary>
     </main>
   );
 }
